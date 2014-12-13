2c81a84898ac062198e32fa35eea18a968383993
Joshua Davey <josh@joshuadavey.com>
2014-12-12 20:50:32 -0600

    Update description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject lein-rb "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Leiningen plugin for integration with Ruby Bundler"
+  :url "http://github.com/jgdavey/lein-rb"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
